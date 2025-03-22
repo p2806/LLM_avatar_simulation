8b9f1e38415e918892ccc45e0e5345a9d87400d0
p2806 <pmadishetty95@gmail.com>
2025-03-22 08:19:46 -0400

    Update database_connection.py

--- database_connection.py
+++ database_connection.py
@@ -13,11 +13,11 @@ import os
 
 def dbconnection():
    
+    load_dotenv()   
     Mongo_URI = os.getenv("MONGO_KEY")
     DBNAME = os.getenv("DBNAME")
     COLLECTION = os.getenv("COLLECTION")
-
-    #Mongo_URI = "mongodb+srv://dbuser:dbuser123@cluster0.0l9sn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+   
     client = MongoClient(Mongo_URI)
     db = client[DBNAME]
     users_collection = db[COLLECTION]
